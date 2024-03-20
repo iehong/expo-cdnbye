327c579ad84a97942f6e368a56ff8e61c66bd824
iehong <iehong@qq.com>
2024-03-20 14:58:32 +0800

    107

--- app.plugin.js
+++ app.plugin.js
@@ -2,8 +2,38 @@ const {
   withAndroidManifest,
   AndroidConfig,
   createRunOncePlugin,
+  withAppBuildGradle,
 } = require("@expo/config-plugins");
 const pkg = require("./package.json");
+function addDependencies(buildGradle) {
+  // 添加需要的仓库地址到repositories部分
+  const newDependencies = `
+  implementation fileTree(includes: ['*.aar','*.jar'],dir: "${resolve(
+    __dirname + "/android/libs"
+  ).replace(/\\/g, "\\\\")}")
+  `;
+  const pattern = /dependencies\s*{\s*/;
+  const match = buildGradle.match(pattern);
+  if (match) {
+    const index = match.index + match[0].length;
+    return (
+      buildGradle.slice(0, index) + newDependencies + buildGradle.slice(index)
+    );
+  } else {
+    return buildGradle + "\n" + newDependencies;
+  }
+}
+function hasDependencies(buildGradle) {
+  if (
+    buildGradle.includes(
+      resolve(__dirname + "/android/libs").replace(/\\/g, "\\\\")
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 const withKey = (config, { token }) => {
   config = withAndroidManifest(config, (config) => {
     const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(
@@ -14,6 +44,12 @@ const withKey = (config, { token }) => {
       "CDNBYE_TOKEN",
       token || "GPYjSOz4g"
     );
+    return config;
+  });
+  config = withAppBuildGradle(config, async (config) => {
+    if (!hasDependencies(config.modResults.contents)) {
+      config.modResults.contents = addDependencies(config.modResults.contents);
+    }
     return config;
   });
   return config;
